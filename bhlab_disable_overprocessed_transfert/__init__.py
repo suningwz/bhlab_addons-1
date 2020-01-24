4d84ebb503076e3a89763ef9be47a530884fba2b
bhlabnadirmessaoudene <46896652+bhlabnadirmessaoudene@users.noreply.github.com>
2020-01-25 00:27:07 +0100

    bhlab 1

---
+++ bhlab_disable_overprocessed_transfert/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+#
+# Copyright (c) 2017  - MLMConseil- www.mlmconseil.dz
+
+from . import models
+
---
+++ bhlab_disable_overprocessed_transfert/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+#
+# Copyright (c) 2017  - MLMConseil- www.mlmconseil.dz
+
+{
+    'name': 'disable overprocessed transfert',
+    'version': '0.2',
+    'category': 'stock',
+    'description': """
+disable overpocessed transfert
+========================================================================
+
+This module applies to companies based in Algeria.
+
+**Email:** it.bhlab@bhinvest.net
+""",
+    'depends': ['stock'],
+    'data': [
+        'views/overprocessed_transfer.xml',
+
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
