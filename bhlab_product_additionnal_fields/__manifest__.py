6b3cd6246e4f2b1459ccb94147560ac33ad4c1e5
bhlabnadirmessaoudene <46896652+bhlabnadirmessaoudene@users.noreply.github.com>
2020-01-25 00:33:59 +0100

    bhlab2

---
+++ bhlab_product_additionnal_fields/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 Simone Rubino - Agile Business Group
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "BHlab product additional informations",
+    "summary": " ",
+    "version": "12.0.1.0.0",
+    "category": "Warehouse",
+    "author": "Nadir",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_view.xml",
+    ]
+}
---
+++ bhlab_product_additionnal_fields/models/product.py
@@ -0,0 +1,43 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import exceptions, fields, models, api, _
+from odoo.exceptions import UserError, ValidationError
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    _type_selection_list = [('none', 'NONE'),('rgt', 'RGT'), ('spp', 'SPP'), ('acc', 'ACC'), ('ctl', 'CTL'), ('con', 'con'), ('app', 'APP'), ('cal', 'CAL'), ('fluid', 'FLUID'), ('ser', 'SER')]
+    _famille_selection_list = [('none','NONE'),('microbiologie','Microbiologie'),('ia_ocd','IA OCD'),('groupage','GROUPAGE'),('ih','IH'),('hba1c','HBA1C'),('ai','AI'),('cc','CC'),('hemostase','HEMOSTASE'),('cc_ocd','CC OCD'),('biomol','BIOMOL'),('hla','HLA'),('ia&cc_ocd','IA&CC OCD'),('ic','IC'),('oncologie','Oncologie')]
+   
+    product_type = fields.Selection(_type_selection_list, string='Type', default='none', required=True, store=True)
+    cdt = fields.Integer(string='CDT', required=True, store=True)
+
+    famille = fields.Selection(_famille_selection_list, string='Famille', default='none', required=True, store=True)
+
+    quantity_pi = fields.Integer(string='Quantity PI', required=True, store=True)
+    tarif_douane = fields.Char(string='Tarif Douanlier', required=True, store=True)
+
+    @api.onchange('cdt')
+    def _compute_CDT(self):
+        if not (self.cdt):
+            return
+        if self.cdt < 0 or self.cdt > 20000 :
+            return {'warning' :{
+                'title':"Incorrect Intiger value",
+                'message':"La valeur est negative ou superieur a 20 000",
+            }}
+
+    @api.onchange('quantity_pi')
+    def _compute_quantity_pi(self):
+        if not (self.quantity_pi):
+            return
+        if self.quantity_pi < 0 :
+            return {'warning' :{
+                'title':"Incorrect Intiger value",
+                'message':"La valeur est negative",
+            }}
+
+    @api.constrains('cdt', 'quantity_pi')
+    def _check_dates(self):
+        if self.cdt < 0 or self.quantity_pi < 0 :
+            raise ValidationError(_('Error ! La valeur de CDT et/ou Quantite PI sont negatives.'))
