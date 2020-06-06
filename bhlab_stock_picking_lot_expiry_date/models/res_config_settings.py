8d2de9aba8cf1488200fe27fc2c7cb6bbd591ba8
nadir messaoudene <it.bhlab@bhinvest.net>
2020-06-06 16:58:05 +0100

    test

--- bhlab_stock_picking_lot_expiry_date/models/res_config_settings.py
+++ bhlab_stock_picking_lot_expiry_date/models/res_config_settings.py
@@ -8,7 +8,7 @@ class StockConfigSettings(models.TransientModel):
 	_inherit = 'res.config.settings'
 
 	product_expiry_alert_time = fields.Integer(string='Product Expiry Alert Days', default=60, help='This is the number of days to alert before product expiry.')
-	
+
 	@api.multi
 	def set_values(self):
 		if self.env.user._is_admin() or self.env['res.users'].has_group('stock.group_production_lot'):
@@ -16,7 +16,7 @@ class StockConfigSettings(models.TransientModel):
 			param = self.env['ir.config_parameter'].sudo()
 			param.set_param('product_expiry_alert_time.product_expiry_alert_time',self.product_expiry_alert_time)
 			return res
-	
+
 	@api.model
 	def get_values(self):
 		res = super(StockConfigSettings, self).get_values()
