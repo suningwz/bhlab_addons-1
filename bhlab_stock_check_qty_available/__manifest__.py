d6dbc0e555ac79c5a4201a39fcaf1dbecd8b7ccd
nadir messaoudene <it.bhlab@bhinvest.net>
2020-07-20 16:58:42 +0100

    stock check qty aialable in sales order line

---
+++ bhlab_stock_check_qty_available/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'bhlab Stock check qty available',
+    'version': '12.0.1.0.0',
+    'category': 'Inventory, Logistic, Storage',
+    'license': 'AGPL-3',
+    'summary': 'Disallow negative stock levels by default',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'www.bhlab.bhinvest.net',
+    'depends': [
+            'sale_stock',
+            ],
+    'data': [
+    ],
+    'installable': True,
+}
