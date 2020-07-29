714278c5f5f02c557551b7d0263a796da04da8a2
nadir messaoudene <it.bhlab@bhinvest.net>
2020-07-29 13:06:21 +0100

    add partner to traceability report

---
+++ bhlab_traceability_lots_report/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'bhlab traceability report',
+    'version': '12.0.1.0.0',
+    'category': 'stock',
+    'author': 'nadir',
+    'website': 'www.bhlab.bhinvest.net',
+    'license': 'AGPL-3',
+    'images': [
+    ],
+    'depends': ['product', 'stock'],
+    'data': [
+        'views/mrp_stock_report_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
