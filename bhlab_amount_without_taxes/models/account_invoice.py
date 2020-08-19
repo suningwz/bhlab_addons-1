4f65b308f1c68b543c82e8914a74e0709d35e5da
nadir messaoudene <it.bhlab@bhinvest.net>
2020-08-19 13:21:48 +0100

    update taxes

--- bhlab_amount_without_taxes/models/account_invoice.py
+++ bhlab_amount_without_taxes/models/account_invoice.py
@@ -10,14 +10,11 @@ class InvoiceOrder(models.Model):
 
     @api.depends('invoice_line_ids')
     def _compute_amount_without_taxes(self):
-        try :
-            self.amount_without_taxes = 0.0
-            self.amount_with_taxes = 0.0
-            for line in self.invoice_line_ids:
-                if len(line.invoice_line_tax_ids) <= 1:
-                    if line.invoice_line_tax_ids.amount == 0.0:
-                        self.amount_without_taxes += line.price_subtotal
-                    else:
-                        self.amount_with_taxes += line.price_subtotal
-        except Exception as e:
-            pass
+        self.amount_without_taxes = 0.0
+        self.amount_with_taxes = 0.0
+        for line in self.invoice_line_ids:
+            if len(line.invoice_line_tax_ids) <= 1:
+                if line.invoice_line_tax_ids.amount == 0.0:
+                    self.amount_without_taxes += line.price_subtotal
+                else:
+                    self.amount_with_taxes += line.price_subtotal
