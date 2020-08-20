1a7dc567dc140e4f7faab10531b1c421b2713032
nadir messaoudene <it.bhlab@bhinvest.net>
2020-08-20 01:08:55 +0100

    Revert "update amount taxes"
    This reverts commit 32d0ff70c35485655502e0d3c754c29776329a4a.

--- bhlab_amount_without_taxes/models/account_invoice.py
+++ bhlab_amount_without_taxes/models/account_invoice.py
@@ -9,21 +9,14 @@ class InvoiceOrder(models.Model):
     @api.multi
     @api.depends('invoice_line_ids')
     def _compute_amount_without_taxes(self):
-
-        amount_without_taxes = 0.0
-        amount_with_taxes = 0.0
         try:
-            for invoice in self:
-                for line in invoice.invoice_line_ids:
-                    if len(line.invoice_line_tax_ids) <= 1:
-                        if line.invoice_line_tax_ids.amount == 0.0:
-                            amount_without_taxes += line.price_subtotal
-                        else:
-                            amount_with_taxes += line.price_subtotal
-
-                invoice.update({
-                    'amount_without_taxes': amount_without_taxes,
-                    'amount_with_taxes': amount_with_taxes
-                })
+            self.amount_without_taxes = 0.0
+            self.amount_with_taxes = 0.0
+            for line in self.invoice_line_ids:
+                if len(line.invoice_line_tax_ids) <= 1:
+                    if line.invoice_line_tax_ids.amount == 0.0:
+                        self.amount_without_taxes += line.price_subtotal
+                    else:
+                        self.amount_with_taxes += line.price_subtotal
         except Exception as e:
             pass
