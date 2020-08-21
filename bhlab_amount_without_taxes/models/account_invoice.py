d3a607062e24d7d66d1cb79bd51fa8003ace571e
nadir messaoudene <it.bhlab@bhinvest.net>
2020-08-21 22:31:55 +0100

    update amount taxes

--- bhlab_amount_without_taxes/models/account_invoice.py
+++ bhlab_amount_without_taxes/models/account_invoice.py
@@ -1,22 +1,22 @@
 from odoo import fields, api, models, _
 
-
 class InvoiceOrder(models.Model):
     _inherit = 'account.invoice'
-    amount_without_taxes = fields.Float(string="Montant sans taxes:", compute='_compute_amount_without_taxes')
-    amount_with_taxes = fields.Float(string="Montant avec taxes:", compute='_compute_amount_without_taxes')
+    amount_without_taxes = fields.Float(string="Montant sans taxes:", store=True, readonly=True, currency_field='currency_id', compute='_compute_amount_without_taxes')
+    amount_with_taxes = fields.Float(string="Montant avec taxes:", store=True, readonly=True, currency_field='currency_id', compute='_compute_amount_without_taxes')
 
     @api.multi
     @api.depends('invoice_line_ids')
     def _compute_amount_without_taxes(self):
         try:
-            self.amount_without_taxes = 0.0
-            self.amount_with_taxes = 0.0
-            for line in self.invoice_line_ids:
-                if len(line.invoice_line_tax_ids) <= 1:
-                    if line.invoice_line_tax_ids.amount == 0.0:
-                        self.amount_without_taxes += line.price_subtotal
-                    else:
-                        self.amount_with_taxes += line.price_subtotal
+            for invoice in self:
+                invoice.amount_without_taxes = 0.0
+                invoice.amount_with_taxes == 0.0
+                for line in invoice.invoice_line_ids:
+                    if len(line.invoice_line_tax_ids) <= 1:
+                        if line.invoice_line_tax_ids.amount == 0.0:
+                            invoice.amount_without_taxes += line.price_subtotal
+                        else:
+                            invoice.amount_with_taxes += line.price_subtotal
         except Exception as e:
             pass
