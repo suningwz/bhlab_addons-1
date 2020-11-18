b979fad212cc99f415fad427f36ed8c76f4e7966
nadir messaoudene <it.bhlab@bhinvest.net>
2020-11-18 11:48:45 +0100

    dowgrade field

--- l10n_dz_base/models/partner.py
+++ l10n_dz_base/models/partner.py
@@ -1,14 +1,13 @@
 ﻿# -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import exceptions, fields, models, api, _
+from odoo.exceptions import UserError, ValidationError
 
 
 class Partner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
-    _activity_sector_selection_list = [('none',''),('PUB','Publique'),('PRIV','Prive')]
-    activity_sector = fields.Selection(_activity_sector_selection_list, string='Secteur d\'activitees', required=True, default='none', store=True)
     code_tiers   = fields.Char('Code tiers')
     # banque_id    = fields.Many2one('res.partner', string='Banque associée')
     employee     = fields.Boolean('Employé')
@@ -19,4 +18,3 @@ class Partner(models.Model):
     ai  = fields.Char('Article d\'imposition')
     # code = fields.Char('Code')  # bessa
     fax = fields.Char('Fax')
-
