56f3e7699f0fbabaf5aadad7a47c9350fdf93515
nadir messaoudene <it.bhlab@bhinvest.net>
2021-01-05 15:24:59 +0100

    add temerature type in product
    add temerature type in product

--- bhlab_product_additionnal_fields/models/product.py
+++ bhlab_product_additionnal_fields/models/product.py
@@ -8,9 +8,11 @@ class ProductTemplate(models.Model):
 
     _type_selection_list = [('none', 'NONE'),('rgt', 'RGT'), ('spp', 'SPP'), ('acc', 'ACC'), ('ctl', 'CTL'), ('cons', 'CONS'), ('app', 'APP'), ('cal', 'CAL'), ('fluid', 'FLUID'), ('ser', 'SER')]
     _famille_selection_list = [('none','NONE'),('microbiologie','Microbiologie'),('ia_ocd','IA OCD'),('groupage','GROUPAGE'),('ih','IH'),('hba1c','HBA1C'),('ai','AI'),('cc','CC'),('hemostase','HEMOSTASE'),('cc_ocd','CC OCD'),('biomol','BIOMOL'),('hla','HLA'),('ia&cc_ocd','IA&CC OCD'),('ic','IC'),('oncologie','Oncologie')]
+    _temperature_selection_list = [('Ambiant', 'Ambiant'),('2-8', '2-8'), ('-20', '-20')]
 
     product_type = fields.Selection(_type_selection_list, string='Type', default='none', store=True)
     cdt = fields.Integer(string='CDT', store=True)
+    temperatur_type = fields.Selection(_temperature_selection_list, string='Temperature', required=True, store=True)
 
     famille = fields.Selection(_famille_selection_list, string='Famille', default='none', store=True)
 
