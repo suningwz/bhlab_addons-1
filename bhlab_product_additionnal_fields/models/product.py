bd02e5b66b1ccb8cbfca2af506a4fa2c70eee5f1
nadir messaoudene <it.bhlab@bhinvest.net>
2021-01-07 14:51:05 +0100

    Update temperature
    Update temperature

--- bhlab_product_additionnal_fields/models/product.py
+++ bhlab_product_additionnal_fields/models/product.py
@@ -8,7 +8,7 @@ class ProductTemplate(models.Model):
 
     _type_selection_list = [('none', 'NONE'),('rgt', 'RGT'), ('spp', 'SPP'), ('acc', 'ACC'), ('ctl', 'CTL'), ('cons', 'CONS'), ('app', 'APP'), ('cal', 'CAL'), ('fluid', 'FLUID'), ('ser', 'SER')]
     _famille_selection_list = [('none','NONE'),('microbiologie','Microbiologie'),('ia_ocd','IA OCD'),('groupage','GROUPAGE'),('ih','IH'),('hba1c','HBA1C'),('ai','AI'),('cc','CC'),('hemostase','HEMOSTASE'),('cc_ocd','CC OCD'),('biomol','BIOMOL'),('hla','HLA'),('ia&cc_ocd','IA&CC OCD'),('ic','IC'),('oncologie','Oncologie')]
-    _temperature_selection_list = [('Ambiant', 'Ambiant'),('2-8', '2-8 °C'), ('-20', '-20 °C')]
+    _temperature_selection_list = [('Ambiant', 'Ambiant'),('2-8', '2-8 °C'), ('-20', '-20 °C'), ('-60', '-60 °C'), ('-80', '-80 °C')]
 
     product_type = fields.Selection(_type_selection_list, string='Type', default='none', store=True)
     cdt = fields.Integer(string='CDT', store=True)
