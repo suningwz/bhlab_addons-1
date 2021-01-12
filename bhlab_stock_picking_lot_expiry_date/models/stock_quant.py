a53ed9975ca4a18c3f5bfc585f6c56b913f76ec2
bhlabnadirmessaoudene <46896652+bhlabnadirmessaoudene@users.noreply.github.com>
2021-01-12 03:16:17 +0100

    Update removal strategy
    Update removal strategy

--- bhlab_stock_picking_lot_expiry_date/models/stock_quant.py
+++ bhlab_stock_picking_lot_expiry_date/models/stock_quant.py
@@ -7,14 +7,36 @@ _logger = logging.getLogger(__name__)
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
+    
+        def _gather(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, strict=False):
+        removal_strategy = self._get_removal_strategy(product_id, location_id)
+        removal_strategy_order = self._get_removal_strategy_order(removal_strategy)
+        domain = [
+            ('product_id', '=', product_id.id),
+        ]
+        if not strict:
+            if lot_id:
+                domain = expression.AND([[('lot_id', '=', lot_id.id)], domain])
+            if package_id:
+                domain = expression.AND([[('package_id', '=', package_id.id)], domain])
+            if owner_id:
+                domain = expression.AND([[('owner_id', '=', owner_id.id)], domain])
+            domain = expression.AND([[('location_id', 'child_of', location_id.id)], domain])
+            domain = expression.AND([[('removal_date', '>', fields.Date.today())], domain])
+        else:
+            domain = expression.AND([[('lot_id', '=', lot_id and lot_id.id or False)], domain])
+            domain = expression.AND([[('package_id', '=', package_id and package_id.id or False)], domain])
+            domain = expression.AND([[('owner_id', '=', owner_id and owner_id.id or False)], domain])
+            domain = expression.AND([[('location_id', '=', location_id.id)], domain])
 
-    expiry_date = fields.Datetime(related='lot_id.expiry_date', widget='Date')
-    ref = fields.Char(string='Lot', related='lot_id.ref')
-    available_qty = fields.Float(string='Qty availble', related='lot_id.available_qty',store=True)
-    
-    
-    @api.model
-    def _get_removal_strategy_order(self, removal_strategy):
-        if removal_strategy == 'fefo':
-            return 'expiry_date, id'
-        return super(StockQuant, self)._get_removal_strategy_order(removal_strategy)
+        # Copy code of _search for special NULLS FIRST/LAST order
+        self.sudo(self._uid).check_access_rights('read')
+        query = self._where_calc(domain)
+        self._apply_ir_rules(query, 'read')
+        from_clause, where_clause, where_clause_params = query.get_sql()
+        where_str = where_clause and (" WHERE %s" % where_clause) or ''
+        query_str = 'SELECT "%s".id FROM ' % self._table + from_clause + where_str + " ORDER BY "+ removal_strategy_order
+        self._cr.execute(query_str, where_clause_params)
+        res = self._cr.fetchall()
+        # No uniquify list necessary as auto_join is not applied anyways...
+        return self.browse([x[0] for x in res])
