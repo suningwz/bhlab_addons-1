fe78d662896cf82984fab139d7c57b00e91645db
bhlabnadirmessaoudene <46896652+bhlabnadirmessaoudene@users.noreply.github.com>
2021-01-13 00:06:30 +0100

    update exipry date
    update exipry date

--- bhlab_stock_picking_lot_expiry_date/models/stock_quant.py
+++ bhlab_stock_picking_lot_expiry_date/models/stock_quant.py
@@ -1,24 +1,29 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import exceptions, fields, models, api, _
+from psycopg2 import OperationalError, Error
+
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
-import logging
-_logger = logging.getLogger(__name__)
+from odoo.osv import expression
+from odoo.tools.float_utils import float_compare, float_is_zero
+
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
-    
+
     expiry_date = fields.Datetime(related='lot_id.expiry_date', widget='Date')
     ref = fields.Char(string='Lot', related='lot_id.ref')
-    available_qty = fields.Float(string='Qty availble', related='lot_id.available_qty',store=True)    
+    available_qty = fields.Float(string='Qty availble', related='lot_id.available_qty',store=True)
+    
     
     @api.model
     def _get_removal_strategy_order(self, removal_strategy):
         if removal_strategy == 'fefo':
             return 'removal_date ASC NULLS FIRST, id'
         return super(StockQuant, self)._get_removal_strategy_order(removal_strategy)
-    
-        def _gather(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, strict=False):
+
+
+    def _gather(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, strict=False):
         removal_strategy = self._get_removal_strategy(product_id, location_id)
         removal_strategy_order = self._get_removal_strategy_order(removal_strategy)
         domain = [
