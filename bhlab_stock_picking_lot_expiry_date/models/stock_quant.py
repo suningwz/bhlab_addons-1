75d58cd738f4a0c5a87d7c54b6f0da440dca5d7f
bhlabnadirmessaoudene <46896652+bhlabnadirmessaoudene@users.noreply.github.com>
2021-01-13 12:46:47 +0100

    update

--- bhlab_stock_picking_lot_expiry_date/models/stock_quant.py
+++ bhlab_stock_picking_lot_expiry_date/models/stock_quant.py
@@ -21,37 +21,3 @@ class StockQuant(models.Model):
         if removal_strategy == 'fefo':
             return 'removal_date ASC NULLS FIRST, id'
         return super(StockQuant, self)._get_removal_strategy_order(removal_strategy)
-
-
-    def _gather(self, product_id, location_id, lot_id=None, package_id=None, owner_id=None, strict=False):
-        removal_strategy = self._get_removal_strategy(product_id, location_id)
-        removal_strategy_order = self._get_removal_strategy_order(removal_strategy)
-        domain = [
-            ('product_id', '=', product_id.id),
-        ]
-        if not strict:
-            if lot_id:
-                domain = expression.AND([[('lot_id', '=', lot_id.id)], domain])
-            if package_id:
-                domain = expression.AND([[('package_id', '=', package_id.id)], domain])
-            if owner_id:
-                domain = expression.AND([[('owner_id', '=', owner_id.id)], domain])
-            domain = expression.AND([[('location_id', 'child_of', location_id.id)], domain])
-            domain = expression.AND([[('removal_date', '>', fields.Date.today())], domain])
-        else:
-            domain = expression.AND([[('lot_id', '=', lot_id and lot_id.id or False)], domain])
-            domain = expression.AND([[('package_id', '=', package_id and package_id.id or False)], domain])
-            domain = expression.AND([[('owner_id', '=', owner_id and owner_id.id or False)], domain])
-            domain = expression.AND([[('location_id', '=', location_id.id)], domain])
-
-        # Copy code of _search for special NULLS FIRST/LAST order
-        self.sudo(self._uid).check_access_rights('read')
-        query = self._where_calc(domain)
-        self._apply_ir_rules(query, 'read')
-        from_clause, where_clause, where_clause_params = query.get_sql()
-        where_str = where_clause and (" WHERE %s" % where_clause) or ''
-        query_str = 'SELECT "%s".id FROM ' % self._table + from_clause + where_str + " ORDER BY "+ removal_strategy_order
-        self._cr.execute(query_str, where_clause_params)
-        res = self._cr.fetchall()
-        # No uniquify list necessary as auto_join is not applied anyways...
-        return self.browse([x[0] for x in res])
