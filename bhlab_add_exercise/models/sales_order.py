4c9db434b776c4ae6aaac8806d4109ce010f53f5
bhlabnadirmessaoudene <it.bhlab@bhinvest.net>
2021-12-19 17:05:35 +0100

    add exercise to SO

---
+++ bhlab_add_exercise/models/sales_order.py
@@ -0,0 +1,12 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields, api
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    exercise = fields.Selection([(2020, '2020'),
+                                (2021, '2021')], required=True)
--- l10n_dz_base/models/partner.py
+++ l10n_dz_base/models/partner.py
@@ -1,20 +1,16 @@
 ﻿# -*- coding: utf-8 -*-
 
 from odoo import exceptions, fields, models, api, _
-from odoo.exceptions import UserError, ValidationError
-
 
 class Partner(models.Model):
     _name = 'res.partner'
     _inherit = 'res.partner'
 
-    code_tiers   = fields.Char('Code tiers')
-    # banque_id    = fields.Many2one('res.partner', string='Banque associée')
-    employee     = fields.Boolean('Employé')
+    code_tiers = fields.Char('Code tiers')
+    employee = fields.Boolean('Employé')
     num_agrement = fields.Char(u'N° CA/Agrement')
-    rc  = fields.Char('R.C')
+    rc = fields.Char('R.C')
     nif = fields.Char('N.I.F', size=15)
     nis = fields.Char('N.I.S', size=15)
-    ai  = fields.Char('Article d\'imposition')
-    # code = fields.Char('Code')  # bessa
+    ai = fields.Char('Article d\'imposition')
     fax = fields.Char('Fax')
